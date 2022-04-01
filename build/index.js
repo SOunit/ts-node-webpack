b44b190eea0eac005c5b608e365385cb537c49dd
SOunit <workbookwork7@gmail.com>
2022-04-01 13:53:26 -0700

    initial commit

---
+++ build/index.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+console.log("test");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQWE7QUFDYiIsInNvdXJjZXMiOlsid2VicGFjazovL3RzLW5vZGUtd2VicGFjay8uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJcInVzZSBzdHJpY3RcIjtcbmNvbnNvbGUubG9nKFwidGVzdFwiKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [{ test: /\.ts$/, use: "ts-loader", exclude: /node_modules/ }],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
